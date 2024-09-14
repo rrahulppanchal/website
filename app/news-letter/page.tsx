3515d25e3b790c2e8bedbcad9eb81e07c1f67da8
Rahul Panchal <rahulpanchaloff@hotmail.com>
2024-09-14 12:17:45 +0000

    misc

--- app/news-letter/page.tsx
+++ app/news-letter/page.tsx
@@ -1,4 +1,4 @@
-import { Text, Title, TextInput, Button, Image } from '@mantine/core';
+import { Text, Title, TextInput, Button, Image, Container } from '@mantine/core';
 import image from '../../assests/icons/email-banner.svg';
 import classes from './NewsLetter.module.css';
 import { HeaderMenu } from '@/components/Header/HeaderMenu';
@@ -8,27 +8,29 @@ export default function NewsLetter() {
     return (
         <>
             <HeaderMenu />
-            <div className={classes.wrapper}>
-                <div className={classes.body}>
-                    <Title className={classes.title}>Wait a minute...</Title>
-                    <Text fw={500} fz="lg" mb={5}>
-                        Subscribe to our newsletter!
-                    </Text>
-                    <Text fz="sm" c="dimmed">
-                        You will never miss important product updates, latest news and community QA sessions. Our
-                        newsletter is once a week, every Sunday.
-                    </Text>
+            <Container size="lg">
+                <div className={classes.wrapper}>
+                    <div className={classes.body}>
+                        <Title className={classes.title}>Wait a minute...</Title>
+                        <Text fw={500} fz="lg" mb={5}>
+                            Subscribe to our newsletter!
+                        </Text>
+                        <Text fz="sm" c="dimmed">
+                            You will never miss important product updates, latest news and community QA sessions. Our
+                            newsletter is once a week, every Sunday.
+                        </Text>
 
-                    <div className={classes.controls}>
-                        <TextInput
-                            placeholder="Your email"
-                            classNames={{ input: classes.input, root: classes.inputWrapper }}
-                        />
-                        <Button className={classes.control}>Subscribe</Button>
+                        <div className={classes.controls}>
+                            <TextInput
+                                placeholder="Your email"
+                                classNames={{ input: classes.input, root: classes.inputWrapper }}
+                            />
+                            <Button className={classes.control}>Subscribe</Button>
+                        </div>
                     </div>
+                    <Image src={image.src} className={classes.image} />
                 </div>
-                <Image src={image.src} className={classes.image} />
-            </div>
+            </Container>
             <Footer />
         </>
     );
--- components/ComingSoon/ComingSoon.tsx
+++ components/ComingSoon/ComingSoon.tsx
@@ -1,25 +1,31 @@
 "use client"
-import { Container, Title, Text, Button, Group } from '@mantine/core';
+import { Container, Title, Text, Button, Group, Image, SimpleGrid } from '@mantine/core';
 import { Illustration } from './Illustration';
 import classes from './ComingSoon.module.css';
 import { useRouter } from 'next/navigation';
+import image from "@/assests/icons/working.svg"
 
 export function ComingSoon() {
     const router = useRouter()
     return (
-        <Container className={classes.root}>
-            <div className={classes.inner}>
-                <Illustration className={classes.image} />
-                <div className={classes.content}>
-                    <Title className={classes.title}>Exciting Things Are on the Horizon!</Title>
-                    <Text c="dimmed" size="lg" ta="center" className={classes.description}>
-                        We're working on an amazing things with innovative features and a sleek design. Stay tuned for updates, and be the first to know when we go live!
+        <Container className={classes.root} size="lg">
+            <SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
+                <Image src={image.src} className={classes.mobileImage} alt="voxq" />
+                <div>
+                    <Title className={classes.title}>Coming Soon...</Title>
+                    <Text c="dimmed" size="lg">
+                        We're working on something truly exciting! Our team is building amazing things with innovative features and a sleek, user-friendly design that will redefine your experience.
                     </Text>
-                    <Group justify="center" onClick={() => router.push('/')}>
-                        <Button size="md">Take me back to home page</Button>
-                    </Group>
+                    <br />
+                    <Text c="dimmed" size="lg">
+                        Stay tuned for updates, and be the first to know when we go live! By subscribing to our updates, you’ll get an inside look at what’s coming and ensure you’re ready to dive into our new features as soon as they launch.
+                    </Text>
+                    <Button variant="outline" size="md" mt="xl" className={classes.control} onClick={() => router.push('/')}>
+                        Get back to home page
+                    </Button>
                 </div>
-            </div>
+                <Image src={image.src} className={classes.desktopImage} alt="voxq" />
+            </SimpleGrid>
         </Container>
     );
 }
